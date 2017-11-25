2d8b86321a3ef68a44ac27cf37c5e6894c33e221
b-i-hide <b.i1993patriots@gmail.com>
2017-11-25 15:27:01 +0900

    [add] sync.WaitGroupを使って、groutineが完了するのを待つように実装

--- main.go
+++ main.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"log"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
+var wg *sync.WaitGroup
+
 func main() {
+	wg = new(sync.WaitGroup)
 
 	for i := 0; i < 1000; i++ {
-		go doSomething()
+		wg.Add(1)
+		go doSomething(wg)
 	}
-
-	time.Sleep(5 * time.Second)
+	wg.Wait()
 	log.Println("end main")
 }
 
 var counter int32 = 0
 
 // 時間がかかるダミー処理
-func doSomething() {
+func doSomething(wg *sync.WaitGroup) {
 	counter := atomic.AddInt32(&counter, 1)
 	log.Printf("start doSomething: %d\n", counter)
-	time.Sleep(4 * time.Second)
+	time.Sleep(5 * time.Second)
 	log.Printf("end doSomething: %d\n", counter)
+	wg.Done()
 }
