4e0fc318e67fb2335d38b75a57e27d05a4683529
b-i-hide <b.i1993patriots@gmail.com>
2017-11-26 14:58:42 +0900

    [add] deferの位置を変更

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	wg := new(sync.WaitGroup)
-	defer wg.Wait()
 
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go doSomething(wg)
 	}
+	wg.Wait()
 	log.Println("end main")
 }
 
@@ -22,9 +22,9 @@ var counter int32 = 0
 
 // 時間がかかるダミー処理
 func doSomething(wg *sync.WaitGroup) {
+	defer wg.Done()
 	counter := atomic.AddInt32(&counter, 1)
 	log.Printf("start doSomething: %d\n", counter)
 	time.Sleep(5 * time.Second)
 	log.Printf("end doSomething: %d\n", counter)
-	wg.Done()
 }
