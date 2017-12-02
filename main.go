5f61e1dd706b1501adb1ca8d7c8643908d13393a
b-i-hide <b.i1993patriots@gmail.com>
2017-12-02 17:34:16 +0900

    [add] channelのcapasityを上限としてタスクを処理する用に実装

--- main.go
+++ main.go
@@ -8,29 +8,41 @@ import (
 )
 
 var wg *sync.WaitGroup
+
 var counter int32 = 0
+
+const (
+	maxCap = 5
+)
 
 func main() {
 	wg := new(sync.WaitGroup)
+	ch := make(chan int, maxCap)
 
-	ch := make(chan int32, 10)
-
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		counter = atomic.AddInt32(&counter, 1)
-		ch <- counter
-		go doSomething(wg, ch)
+	for i := 0; i < maxCap; i++ {
+		go func() {
+			for _ = range ch {
+				doSomething(wg, ch)
+			}
+		}()
 	}
-	close(ch)
+
+	for i := 0; i < 20; i++ {
+		// WGをインクリメント
+		wg.Add(1)
+		ch <- i
+	}
+
 	wg.Wait()
 	log.Println("end main")
 }
 
 // 時間がかかるダミー処理
-func doSomething(wg *sync.WaitGroup, ch chan int32) {
-	counter := <-ch
+func doSomething(wg *sync.WaitGroup, ch chan int) {
+	// WGをデクリメント
 	defer wg.Done()
+	counter := atomic.AddInt32(&counter, 1)
 	log.Printf("start doSomething: %d\n", counter)
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
 	log.Printf("end doSomething: %d\n", counter)
 }
