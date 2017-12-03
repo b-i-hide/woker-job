fc7e6b17ec4e135ea66c29791189fae1f0144b7f
b-i-hide <b.i1993patriots@gmail.com>
2017-12-03 17:58:07 +0900

    [add] dispatcher構造体の追加、queueを用いてworkerを制御する

--- main.go
+++ main.go
@@ -7,49 +7,54 @@ import (
 	"time"
 )
 
+var counter int32 = 0
+
 type Dispatcher struct {
-	queue chan int32
+	queue chan int
 	wg    sync.WaitGroup
 }
 
-var wg *sync.WaitGroup
-var counter int32 = 0
-
 const (
-	maxWorkers = 3
+	maxWorkers = 20
 	maxQueues  = 100
 )
 
 func main() {
 	d := NewDispatcher()
-
-	for v := 0; v < maxWorkers; v++ {
-		go doSomething(wg, d)
-	}
+	d.Start()
 
 	for i := 0; i < 1000; i++ {
-		d.wg.Add(maxWorkers)
-		counter = atomic.AddInt32(&counter, 1)
-		d.queue <- counter
-
+		d.queue <- i
 	}
+
 	close(d.queue)
 	d.wg.Wait()
 	log.Println("end main")
 }
 
+func NewDispatcher() *Dispatcher {
+	d := &Dispatcher{
+		queue: make(chan int, maxQueues),
+	}
+	return d
+}
+
+func (d *Dispatcher) Start() {
+	d.wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		go func() {
+			defer d.wg.Done()
+			for range d.queue {
+				doSomething()
+			}
+		}()
+	}
+}
+
 // 時間がかかるダミー処理
-func doSomething(wg *sync.WaitGroup, d *Dispatcher) {
-	counter := <-d.queue
+func doSomething() {
+	counter := atomic.AddInt32(&counter, 1)
 	log.Printf("start doSomething: %d\n", counter)
 	time.Sleep(5 * time.Second)
 	log.Printf("end doSomething: %d\n", counter)
-	d.wg.Done()
-}
-
-func NewDispatcher() *Dispatcher {
-	d := &Dispatcher{
-		queue: make(chan int32, maxQueues),
-	}
-	return d
 }
